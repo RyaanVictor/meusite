4acd452f52ed12c86ceae10a1a8e55e9d3e4c024
ryan <104521089+RyaanVictor@users.noreply.github.com>
2023-01-30 15:21:12 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -4,12 +4,12 @@
 			<title>Teste da Linguagem PHP</title>
 			<meta charset="utf-8">
 		</head>
-		<body>
+		<body style="background-color:powderblue;"></body>
 			<?php 
 				echo '<h1>Teste da Linguagem HTML (HyperText Markup Language)</h1>';
 				echo 'Autor: Ryan Victor<br>';
 				echo 'Instagram: https://www.instagram.com/imnotryanzx/?next=%2F <br>';
-				echo 'GitHub : github.com/RyaanVictor <br>'
+				echo 'GitHub : github.com/RyaanVictor <br>';
 			?>
 		</body>
 	</html>
