758fa8cae090138fc78e3277239de38fae21e334
ryan <104521089+RyaanVictor@users.noreply.github.com>
2023-01-30 15:13:25 -0300

    Create teste.php

---
+++ teste.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+	<html lang="pt-br">
+		<head>
+			<title>Teste da Linguagem PHP</title>
+			<meta charset="utf-8">
+		</head>
+		<body>
+			<?php 
+				echo '<h1>Teste da Linguagem HTML (HyperText Markup Language)</h1>';
+				echo 'Autor: Ryan Victor<br>';
+				echo 'Instagram: https://www.instagram.com/imnotryanzx/?next=%2F <br>';
+				echo 'GitHub : github.com/RyaanVictor <br>'
+			?>
+		</body>
+	</html>
